bcb8233d5ec7a3875aaa0bca8c9304872cf8f880
roman <roman3668@gmail.com>
2022-11-07 14:54:25 +0300

    Add Task 3

--- Homework3 C#/Program.cs
+++ Homework3 C#/Program.cs
@@ -1,12 +1,9 @@
-﻿Console.WriteLine("Enter point 1 coordinates (X,Y,Z)");
-int num1X = Convert.ToInt32(Console.ReadLine());
-int num1Y = Convert.ToInt32(Console.ReadLine());
-int num1Z = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Enter number:");
+int num = int.Parse(Console.ReadLine());
+int i = 1;
 
-Console.WriteLine("Enter point 2 coordinates (X,Y,Z)");
-int num2X = Convert.ToInt32(Console.ReadLine());
-int num2Y = Convert.ToInt32(Console.ReadLine());
-int num2Z = Convert.ToInt32(Console.ReadLine());
-
-double dist = Math.Sqrt(Math.Pow((num2X-num1X),2) + Math.Pow((num2Y-num1Y),2) + Math.Pow((num2Z - num1Z),2));
-System.Console.WriteLine($"Distance is {dist}");
+while (i <= num)
+{
+    System.Console.Write($"{i*i*i}, ");
+    i++;
+}
